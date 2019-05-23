d09798dee90a41135ce4da6fd3e4a37306e7f250
Julliya <gorsheninajulliya@gmail.com>
2019-05-23 22:04:15 +0300

    Add test

---
+++ src/main/java/pages/SearchPage.java
@@ -0,0 +1,31 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SearchPage {
+
+    private By SearchFild = By.id("text");
+    private By SearchButton = By.cssSelector("[type='submit']");
+    private By FirstLink = By.cssSelector("[accesskey='1']");
+
+    private final WebDriver driver;
+
+    public SearchPage(WebDriver driver){
+        this.driver=driver;
+    }
+
+    public void waitForElement(By locator){
+        (new WebDriverWait(driver, 10))
+                .until (ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    public void Search(String text){
+        waitForElement(SearchFild);
+        driver.findElement(SearchFild).sendKeys(text);
+        driver.findElement(SearchButton).click();
+    }
+    
+}
